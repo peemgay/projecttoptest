f539fb5127f2ee5c39f7e0bdb865b813af0034ab
Mona Lisa <peempeem2@gmail.com>
2020-02-27 21:40:31 +0700

    Save

---
+++ ValuaTaxi/ValuaTaxi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ValuaTaxi
+//
+//  Created by Peem on 27/2/2563 BE.
+//  Copyright © 2563 panuwatprojact. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
